f4fe255d2254f2793865dcbd9e07fa3ecc081f4a
yatorooo <brett.zju@gmail.com>
2025-09-24 17:46:42 +0100

    pricer cpp update

--- src/pricer.cpp
+++ src/pricer.cpp
@@ -1,71 +1,88 @@
+// pricer.cpp
 #include "pricer.hpp"
 #include <ql/quantlib.hpp>
-#include <sstream>
+#include <ql/utilities/dataparsers.hpp>  // DateParser::parseISO
 #include <cmath>
 
 using namespace QuantLib;
 
-static Date parse_date(const std::string& s) {
-    int y=0,m=0,d=0; char dash;
-    std::stringstream ss(s);
-    ss >> y >> dash >> m >> dash >> d;
-    return Date(d, (Month)m, y);
-}
-
 ResultRow price_option(const OptionRow& r) {
-    Calendar cal = UnitedKingdom();
-    DayCounter dc = Actual365Fixed();
+    // --- Day count / calendar & evaluation date ---
+    Calendar   cal = UnitedKingdom();
+    DayCounter dc  = Actual365Fixed();
 
-    Date tradeDate = parse_date(r.trade_date);
+    Date tradeDate = DateParser::parseISO(r.trade_date);
     Settings::instance().evaluationDate() = tradeDate;
 
-    Handle<Quote> spot(boost::shared_ptr<Quote>(new SimpleQuote(r.spot)));
-    Handle<YieldTermStructure> riskFree(boost::shared_ptr<YieldTermStructure>(
-        new FlatForward(tradeDate, r.r, dc)));
-    Handle<YieldTermStructure> dividend(boost::shared_ptr<YieldTermStructure>(
-        new FlatForward(tradeDate, r.q, dc)));
-    Handle<BlackVolTermStructure> volTS(boost::shared_ptr<BlackVolTermStructure>(
-        new BlackConstantVol(tradeDate, cal, 0.20, dc)));
+    // --- Spot & curves (use QuantLib::ext smart pointers) ---
+    ext::shared_ptr<Quote> spotPtr = ext::make_shared<SimpleQuote>(r.spot);
+    Handle<Quote> spot(spotPtr);
 
-    boost::shared_ptr<BlackScholesMertonProcess> process(
-        new BlackScholesMertonProcess(spot, dividend, riskFree, volTS));
-    boost::shared_ptr<PricingEngine> engine(new AnalyticEuropeanEngine(process));
+    ext::shared_ptr<YieldTermStructure> riskFreePtr =
+        ext::make_shared<FlatForward>(tradeDate, r.r, dc);
+    Handle<YieldTermStructure> riskFree(riskFreePtr);
 
+    ext::shared_ptr<YieldTermStructure> dividendPtr =
+        ext::make_shared<FlatForward>(tradeDate, r.q, dc);
+    Handle<YieldTermStructure> dividend(dividendPtr);
+
+    ext::shared_ptr<BlackVolTermStructure> volPtr =
+        ext::make_shared<BlackConstantVol>(tradeDate, cal, 0.20, dc);
+    Handle<BlackVolTermStructure> volTS(volPtr);
+
+    // --- Process & pricing engine ---
+    auto process = ext::make_shared<BlackScholesMertonProcess>(
+        spot, dividend, riskFree, volTS);
+    auto engine  = ext::make_shared<AnalyticEuropeanEngine>(process);
+
+    // --- Build European option ---
     Option::Type typ = (r.type == 'C' ? Option::Call : Option::Put);
-    Date maturity = parse_date(r.maturity_date);
-    boost::shared_ptr<Exercise> exercise(new EuropeanExercise(maturity));
-    boost::shared_ptr<StrikedTypePayoff> payoff(new PlainVanillaPayoff(typ, r.strike));
+    Date maturity = DateParser::parseISO(r.maturity_date);
+
+    ext::shared_ptr<Exercise> exercise =
+        ext::make_shared<EuropeanExercise>(maturity);
+    ext::shared_ptr<StrikedTypePayoff> payoff =
+        ext::make_shared<PlainVanillaPayoff>(typ, r.strike);
+
     EuropeanOption opt(payoff, exercise);
     opt.setPricingEngine(engine);
 
+    // --- Time to maturity (year fraction) ---
     Time T = dc.yearFraction(tradeDate, maturity);
-    if (T <= 0.0) T = 1.0/365.0;
+    if (T <= 0.0) T = 1.0/365.0;  // guard for same-day etc.
 
+    // --- Implied vol from market price ---
     Real iv = 0.20;
     try {
-        iv = opt.impliedVolatility(r.market_price, process, 1e-7, 100, 1e-6, 5.0);
+        iv = opt.impliedVolatility(
+            r.market_price, process,
+            1e-7,      // accuracy
+            100,       // max evaluations
+            1e-6, 5.0  // minVol, maxVol
+        );
     } catch (...) {
-        // fallback: keep 20%
+        // keep default 20% if it fails
     }
 
-    Handle<BlackVolTermStructure> solvedVol(boost::shared_ptr<BlackVolTermStructure>(
-        new BlackConstantVol(tradeDate, cal, iv, dc)));
-    process = boost::shared_ptr<BlackScholesMertonProcess>(
-        new BlackScholesMertonProcess(spot, dividend, riskFree, solvedVol));
-    engine.reset(new AnalyticEuropeanEngine(process));
+    // --- Rebuild with solved vol and reprice/Greeks ---
+    Handle<BlackVolTermStructure> solvedVol(
+        ext::make_shared<BlackConstantVol>(tradeDate, cal, iv, dc));
+    process = ext::make_shared<BlackScholesMertonProcess>(
+        spot, dividend, riskFree, solvedVol);
+    engine  = ext::make_shared<AnalyticEuropeanEngine>(process);
     opt.setPricingEngine(engine);
 
+    // --- Pack results ---
     ResultRow res;
-    res.opt = r;
-    res.ttm = T;
-    res.iv = iv;
+    res.opt         = r;
+    res.ttm         = T;
+    res.iv          = iv;
     res.model_price = opt.NPV();
-    res.error = res.model_price - r.market_price;
-    res.delta = opt.delta();
-    res.gamma = opt.gamma();
-    res.vega  = opt.vega();
-    res.theta = opt.theta();
-    res.rho   = opt.rho();
-
+    res.error       = res.model_price - r.market_price;
+    res.delta       = opt.delta();
+    res.gamma       = opt.gamma();
+    res.vega        = opt.vega();
+    res.theta       = opt.theta();
+    res.rho         = opt.rho();
     return res;
 }
